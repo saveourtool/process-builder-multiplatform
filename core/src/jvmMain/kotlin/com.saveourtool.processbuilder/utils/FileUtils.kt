93b956b7288a6b9aa5ca321302938c0793320a31
sanyavertolet <lxnfrolov@gmail.com>
2023-04-12 13:02:48 +0300

    Reuse ProcessBuilder from save-cli
    ### What's done:
     * replaced myDeleteRecursively with deleteRecursively from okio
    
    (#3)

--- core/src/jvmMain/kotlin/com.saveourtool.processbuilder/utils/FileUtils.kt
+++ core/src/jvmMain/kotlin/com.saveourtool.processbuilder/utils/FileUtils.kt
@@ -5,14 +5,5 @@
 package com.saveourtool.processbuilder.utils
 
 import okio.FileSystem
-import okio.Path
-import java.nio.file.Files
 
 actual val fs: FileSystem = FileSystem.SYSTEM
-
-actual fun FileSystem.myDeleteRecursively(path: Path) {
-    path.toFile().walkBottomUp().forEach { file ->
-        fileUtilsLogger.trace { "Attempt to delete file $file" }
-        Files.delete(file.toPath())
-    }
-}
